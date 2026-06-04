4a1cbe3968cca367fc6a514778eed8d87adfedac
Shafeeq <shafeeqsadhic2003@gmail.com>
2026-06-04 13:10:55 +0530

    Initial commit - Face Emotion Recognition Project

---
+++ realtime.py
@@ -0,0 +1,84 @@
+import cv2
+import numpy as np
+from tensorflow.keras.models import load_model
+
+# Load trained model
+model = load_model('models/emotion_model.h5')
+
+# Emotion labels
+emotion_labels = [
+    'Angry',
+    'Disgust',
+    'Fear',
+    'Happy',
+    'Neutral',
+    'Sad',
+    'Surprise'
+]
+
+# Load Haar Cascade
+face_classifier = cv2.CascadeClassifier(
+    cv2.data.haarcascades +
+    'haarcascade_frontalface_default.xml'
+)
+
+# Start webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_classifier.detectMultiScale(
+        gray,
+        scaleFactor=1.3,
+        minNeighbors=5
+    )
+
+    for (x, y, w, h) in faces:
+
+        roi_gray = gray[y:y+h, x:x+w]
+
+        roi_gray = cv2.resize(
+            roi_gray,
+            (48,48)
+        )
+
+        roi = roi_gray.astype('float') / 255.0
+
+        roi = np.expand_dims(roi, axis=0)
+        roi = np.expand_dims(roi, axis=-1)
+
+        prediction = model.predict(roi)
+
+        max_index = int(np.argmax(prediction))
+
+        emotion = emotion_labels[max_index]
+
+        cv2.rectangle(
+            frame,
+            (x,y),
+            (x+w,y+h),
+            (255,0,0),
+            2
+        )
+
+        cv2.putText(
+            frame,
+            emotion,
+            (x, y-10),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.9,
+            (0,255,0),
+            2
+        )
+
+    cv2.imshow('Emotion Detector', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ train.py
@@ -0,0 +1,126 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.callbacks import EarlyStopping
+import matplotlib.pyplot as plt
+
+# Dataset Paths
+train_dir = 'dataset/train'
+test_dir = 'dataset/test'
+
+# Data preprocessing
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=30,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.3,
+    horizontal_flip=True,
+    brightness_range=[0.8, 1.2]
+)
+
+test_datagen = ImageDataGenerator(
+    rescale=1./255
+)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(48, 48),
+    batch_size=64,
+    color_mode='rgb',
+    class_mode='categorical'
+)
+
+test_generator = test_datagen.flow_from_directory(
+    test_dir,
+    target_size=(48, 48),
+    batch_size=64,
+    color_mode='grayscale',
+    class_mode='categorical'
+)
+
+# CNN Model
+model = Sequential()
+
+# Layer 1
+model.add(Conv2D(
+    32,
+    kernel_size=(3,3),
+    activation='relu',
+    input_shape=(48,48,3)
+))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+# Layer 2
+model.add(Conv2D(
+    64,
+    kernel_size=(3,3),
+    activation='relu'
+))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+# Layer 3
+model.add(Conv2D(
+    128,
+    kernel_size=(3,3),
+    activation='relu'
+))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Conv2D(256, (3,3), activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(2,2))
+
+# Flatten
+model.add(Flatten())
+
+# Fully Connected Layer
+model.add(Dense(256, activation='relu'))
+
+# Dropout
+model.add(Dropout(0.5))
+
+# Output Layer
+model.add(Dense(7, activation='softmax'))
+
+# Compile model
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(
+    learning_rate=0.0001
+)
+early_stop = EarlyStopping(
+    monitor='val_loss',
+    patience=5,
+    restore_best_weights=True
+)
+
+# Train Model
+history = model.fit(
+    train_generator,
+    validation_data=test_generator,
+    epochs=50,
+    callbacks=[early_stop]
+)
+
+# Save Model
+model.save('models/emotion_model.h5')
+
+print("Model Trained Successfully!")
+
+# Plot Accuracy Graph
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+
+plt.legend(['Train', 'Validation'])
+
+plt.show()
